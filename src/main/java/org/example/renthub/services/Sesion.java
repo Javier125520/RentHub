66fca64f033dc05057852a4df068aa9ad9459641
JavierGuerrero <jguehur988@g.educaand.es>
2026-05-19 18:17:19 +0200

    Commit 17

--- src/main/java/org/example/renthub/services/Sesion.java
+++ src/main/java/org/example/renthub/services/Sesion.java
@@ -6,20 +6,8 @@ public class Sesion {
 
     private static Usuario usuarioActual;
 
-    public static void iniciarSesion(Usuario usuario) {
-        usuarioActual = usuario;
-    }
-
     public static Usuario getUsuario() {
         return usuarioActual;
-    }
-
-    public static boolean estaLogueado() {
-        return usuarioActual != null;
-    }
-
-    public static void cerrarSesion() {
-        usuarioActual = null;
     }
 
     public static void setUsuario(Usuario usuario) {
